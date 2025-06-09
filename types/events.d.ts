c0676ade13109737e9bfc58e8e948b329f1d818f
YOUNGHO0 <31pqopq13@gmail.com>
2025-06-09 21:51:23 +0900

    Feat : 채널 생성 이벤트 추가

--- types/events.d.ts
+++ types/events.d.ts
@@ -2,3 +2,11 @@ export interface UserSpaceInEvent {
     type:"spaceIn"
     spaceId:number
 }
+
+export interface ChannelCreateEvent {
+    type:"channelCreate"
+    message:{
+        name : string
+        userList : number[]
+    }
+}
