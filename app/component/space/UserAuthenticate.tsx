0a69ae705f93c5b3f398314352481ac977534a31
YOUNGHO0 <31pqopq13@gmail.com>
2025-06-09 21:55:16 +0900

    Feat : 사용자 권한 체크 후 없으면 login으로 리다이렉트

---
+++ app/component/space/UserAuthenticate.tsx
@@ -0,0 +1,16 @@
+'use client'
+import axios, {AxiosResponse} from "axios";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+
+export default ()=>{
+    const router = useRouter();
+    useEffect(() => {
+        axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user`,{withCredentials:true, headers:{"Content-Type":"application/json"}})
+            .then((response:AxiosResponse) => {
+                if(response.status !== 200) router.push(`${process.env.NEXT_PUBLIC_WEB_URL}/login`)
+            })
+            .catch(()=>{router.push(`${process.env.NEXT_PUBLIC_WEB_URL}/login`)})
+    }, []);
+
+}
