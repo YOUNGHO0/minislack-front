d9d0fef772e9c46b0a51dd6eea05ffcf10eac7d7
YOUNGHO0 <31pqopq13@gmail.com>
2025-06-11 20:14:30 +0900

    Remove : 불필요한 코드 삭제

--- app/space/[space]/main/page.tsx
+++ app/space/[space]/main/page.tsx
@@ -5,11 +5,6 @@ import ChannelList from "@/app/component/channellist/ChannelList";
 import {PlusIcon} from "@heroicons/react/24/outline";
 export default function Home() {
 
-    let data: Channel[] = [
-        { id: 1, name: "hello talk" },
-        { id: 2, name: "hi good" },
-        { id: 3, name: "good better" }
-    ];
     return (<></>
     );
 }
