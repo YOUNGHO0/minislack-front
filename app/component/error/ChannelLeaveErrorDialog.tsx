476d5e1753760c43ae3a98927832a0002c7c7b6b
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-04 18:04:08 +0900

    Feat : 채널, 스페이스 에러 창 추가 * 관리자가 아닌 상태에서 나갈 수 있음

---
+++ app/component/error/ChannelLeaveErrorDialog.tsx
@@ -0,0 +1,24 @@
+import {AlertDialog, Button, Flex} from "@radix-ui/themes";
+import * as React from "react";
+
+export default  ({alertState, closeWindow}: {alertState:boolean, closeWindow: ()=>void}) =>{
+
+    return (        <AlertDialog.Root open={alertState} >
+        <AlertDialog.Content maxWidth="450px">
+            <AlertDialog.Title>관리자 변경 필요</AlertDialog.Title>
+            <AlertDialog.Description size="2">
+                <div>채널 관리자는 나갈 수 없습니다.</div>
+                <div>다른 사람으로 관리자를 변경하세요</div>
+            </AlertDialog.Description>
+
+            <Flex gap="3" mt="4" justify="end">
+                <AlertDialog.Cancel>
+                    <Button variant="solid" color="blue"
+                            onClick={closeWindow}
+                    >확인
+                    </Button>
+                </AlertDialog.Cancel>
+            </Flex>
+        </AlertDialog.Content>
+    </AlertDialog.Root>)
+}
