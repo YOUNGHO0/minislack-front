bae04128c5f5060edf1f559c5754eb3d6ee0e03f
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-04 18:05:00 +0900

    Feat : 채널/ 스페이스 나가는 다이얼로그 창 생성

---
+++ app/component/channel/ChannelLeaveDialog.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import {AlertDialog, Button, Flex} from "@radix-ui/themes";
+
+export default ({alertState, closeWindow, leave} : {alertState : boolean, closeWindow : ()=>void, leave: ()=>void}) => {
+    return (
+        <AlertDialog.Root open={alertState} >
+            <AlertDialog.Content maxWidth="450px">
+                <AlertDialog.Title>Revoke access</AlertDialog.Title>
+                <AlertDialog.Description size="2">
+                    채널을 나가시겠습니까?
+                </AlertDialog.Description>
+
+                <Flex gap="3" mt="4" justify="end">
+                    <AlertDialog.Cancel>
+                        <Button variant="soft" color="gray"
+                                onClick={closeWindow}>
+                            취소
+                        </Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button variant="solid" color="red"
+                                onClick={()=>{ closeWindow(); leave();}}>
+                            채널 나가기
+                        </Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
+    );
+};
