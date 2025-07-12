a412d64fa42cf7029a0cee1d1821d69d424f469b
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-12 09:52:41 +0900

    Fix : 로그인 페이지 content로 분리

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,18 @@
 'use client';
-import React from 'react';
-import GoogleIcon from "@/app/login/GoogleIcon";
-import {Heading, Separator} from "@radix-ui/themes";
-import {useSearchParams} from "next/navigation";
 
-export default function LoginPage() {
-    const handleGoogleLogin = () => {
-        console.log('Google 로그인 클릭됨');
-    };
-    const searchParams = useSearchParams()
-    const redirect = searchParams.get('redirect')
-    const href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google${redirect ? `?redirect=${redirect}` : ''}`
+import React, { Suspense } from 'react';
+import GoogleIcon from "@/app/login/GoogleIcon";
+import { Heading, Separator } from "@radix-ui/themes";
+import { useSearchParams } from "next/navigation";
+
+function LoginContent() {
+    const searchParams = useSearchParams();
+    const redirect = searchParams.get('redirect');
+    const href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google${redirect ? `?redirect=${redirect}` : ''}`;
 
     return (
         <div className="w-full flex items-center justify-center">
-
             <div className="pt-10 px-5 w-full max-w-md flex flex-col justify-center">
-
                 <Heading className="pb-5">로그인</Heading>
                 <div className="flex items-center w-full pb-3">
                     <Separator className="flex-grow border-t border-gray-300"/>
@@ -24,11 +20,17 @@ export default function LoginPage() {
                     <Separator className="flex-grow border-t border-gray-300"/>
                 </div>
                 <a href={href}>
-                    <GoogleIcon/>
+                    <GoogleIcon />
                 </a>
-
             </div>
         </div>
+    );
+}
 
+export default function LoginPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <LoginContent />
+        </Suspense>
     );
 }
