09623c10a2c934d6380bc81614e16aa756deb6fd
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-12 21:53:22 +0900

    Feat : main 페이지가 아닐 경우 main페이지로 이동

--- app/space/[space]/page.tsx
+++ app/space/[space]/page.tsx
@@ -1,4 +1,16 @@
+'use client'
+import {useEffect} from "react";
+import {usePathname, useRouter} from "next/navigation";
+
 export default ()=>{
 
-    return <div> helloo </div>
+    const pathname = usePathname();
+    const router = useRouter();
+    useEffect(() => {
+        if (!pathname.endsWith("/main")) {
+            router.replace(pathname + "/main");
+        }
+    }, []);
+
+    return <></>
 }
