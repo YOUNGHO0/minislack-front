540f744bfe3daedc344330de6e1967dcea367925
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-13 18:48:30 +0900

    Style : 간격 줄바꿈

--- types/events.d.ts
+++ types/events.d.ts
@@ -1,110 +1,111 @@
 import {User} from "@/types/type";
 
 export interface UserSpaceInEvent {
-    type:"spaceIn"
-    spaceId:number
+    type: "spaceIn"
+    spaceId: number
 }
 
 export interface ChannelCreateSendEvent {
-    type:"channelCreate"
-    message:{
-        name : string
-        userList : number[]
+    type: "channelCreate"
+    message: {
+        name: string
+        userList: number[]
         privateChannel: boolean
         externalBlocked: boolean
     }
 }
 
 export interface ChannelUpdateSendEvent {
-    type:"channelUpdate"
-    message:{
-        id:number
-        channelName:string
+    type: "channelUpdate"
+    message: {
+        id: number
+        channelName: string
         privateChannel: boolean
         externalBlocked: boolean
     }
 }
 
 export interface ChannelDeleteSendEvent {
-    type:"channelDelete"
-    message:{
-        id:number
-}
-}
-
-export interface ChannelCreateReceiveEvent{
-    type:"channelCreate"
-    id :number
-    channelName : string
-
+    type: "channelDelete"
+    message: {
+        id: number
+    }
 }
 
-export interface ChannelUpdateReceiveEvent{
-    type : "channelUpdate"
-    id :number
-    channelName : string
+export interface ChannelCreateReceiveEvent {
+    type: "channelCreate"
+    id: number
+    channelName: string
+
+}
+
+export interface ChannelUpdateReceiveEvent {
+    type: "channelUpdate"
+    id: number
+    channelName: string
     privateChannel: boolean
 }
 
 export interface ChannelDeleteReceiveEvent {
-    type : "channelDelete"
-    id :number
+    type: "channelDelete"
+    id: number
 }
 
 export interface ChatCreateSendEvent {
-    type:"chatCreate"
-    message:{
-        channelId : number
-        parent : number
-        text : string
+    type: "chatCreate"
+    message: {
+        channelId: number
+        parent: number
+        text: string
     }
 }
 
 export interface MessageEditSendEvent {
-    type:"chatUpdate"
-    message:{
-        channelId : number
-        chatId : number
-        text : string
+    type: "chatUpdate"
+    message: {
+        channelId: number
+        chatId: number
+        text: string
     }
 }
+
 export interface MessageDeleteSendEvent {
-    type:"chatDelete"
-    message:{
-        channelId : number
-        id : number
+    type: "chatDelete"
+    message: {
+        channelId: number
+        id: number
     }
 }
 
 
+export interface ChannelJoinSend {
 
-export interface ChannelJoinSend{
-
-    type:"channelJoin"
-    message:{
-        id : number
+    type: "channelJoin"
+    message: {
+        id: number
     }
 }
 
 
 export interface ChatCreateReceiveEvent {
-    type:"chatCreate"
+    type: "chatCreate"
     chatMessage: string
-    id : number
+    id: number
+    parentId: number
     createdDate: string
     channelId : number
     user:User
 }
 
 export interface ChatDeleteReceiveEvent {
-    type:"chatDelete"
-    id : number
-    channelId : number
+    type: "chatDelete"
+    id: number
+    channelId: number
 }
 
 export interface ChatUpdateReceiveEvent {
-    type:"chatUpdate"
+    type: "chatUpdate"
     text: string
-    id : number
-    channelId : number
+    id: number
+    channelId: number
 }
