7dffbb6a02e39c45b9743c806ea882b44ef8c293
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-16 16:46:01 +0900

    Feat : 버튼에 닫기 이벤트 바인딩

--- app/component/channel/ChannelAdminChange.tsx
+++ app/component/channel/ChannelAdminChange.tsx
@@ -156,6 +156,7 @@ const AdminChangeDialog = ({open, closeWindow} : {open : boolean , closeWindow :
 
                     <Dialog.Close asChild>
                         <button
+                            onClick={closeWindow}
                             className="absolute right-2.5 top-2.5 inline-flex size-[25px] appearance-none items-center justify-center rounded-full text-violet11 bg-gray3 hover:bg-violet4 focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none"
                             aria-label="Close"
                         >
