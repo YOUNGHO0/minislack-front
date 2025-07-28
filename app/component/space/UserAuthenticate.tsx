4372911727732ee81d4c131c5e89b1c73f5092c3
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-28 16:41:03 +0900

    Fix : auth 인증을 다른곳에도 사용하기 위해 분리

--- app/component/space/UserAuthenticate.tsx
+++ app/component/space/UserAuthenticate.tsx
@@ -2,18 +2,15 @@
 import axios, {AxiosResponse} from "axios";
 import {useParams, usePathname, useRouter} from "next/navigation";
 import {useEffect} from "react";
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
+import {checkUserAuth} from "@/utils/auth"
+
 
 export default ()=>{
     const router = useRouter();
     const pathname = usePathname();
     useEffect(() => {
-        axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user`,{withCredentials:true, headers:{"Content-Type":"application/json"}})
-            .then((response:AxiosResponse) => {
-                if(response.status !== 200) router.push(`${process.env.NEXT_PUBLIC_WEB_URL}/login`)
-            })
-            .catch(()=>{
-
-                router.push(`${process.env.NEXT_PUBLIC_WEB_URL}/login?redirect=${pathname}`)})
+        checkUserAuth(router, pathname);
     }, []);
 
 }
