f8a1c17e916f02209d8efdff34b639193080a829
YOUNGHO0 <31pqopq13@gmail.com>
2025-07-29 19:53:04 +0900

    Fix : 모바일 버전 패딩 추가

--- app/component/NavBar.tsx
+++ app/component/NavBar.tsx
@@ -44,7 +44,7 @@ const ImprovedNavbar = () => {
             </nav>
 
             {/* Mobile Navigation */}
-            <nav className="lg:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-30">
+            <nav className="lg:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg pt-1 z-30">
                 <div className="flex items-center justify-around">
                     {menuItems.map(({ id, icon: Icon, label, path }) => (
                         <button
