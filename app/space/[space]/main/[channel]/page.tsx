bace5932820a71e2faa36b076da206a9379ac705
YOUNGHO0 <31pqopq13@gmail.com>
2025-08-07 19:59:49 +0900

    Fix : overflowy- hidden 적용

--- app/space/[space]/main/[channel]/page.tsx
+++ app/space/[space]/main/[channel]/page.tsx
@@ -539,7 +539,7 @@ export default () => {
 
 
 
-    return <div className=""
+    return <div className="overflow-y-hidden"
         style={{  height: `calc(100dvh - ${keyboardHeight}px)`,}}
     >
         <div className="flex bg-nav py-1 px-2 font-bold items-center gap-2  top-0 left-0 right-0 z-[80]"
