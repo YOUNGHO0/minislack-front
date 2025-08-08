0a1b09d2265927851aac7056ecbd555cd676f8e1
YOUNGHO0 <31pqopq13@gmail.com>
2025-08-08 19:00:11 +0900

    Fix : transition 임시 주석처리

--- app/space/[space]/main/[channel]/page.tsx
+++ app/space/[space]/main/[channel]/page.tsx
@@ -578,7 +578,7 @@ export default () => {
     return <div className="flex flex-col h-screen"
                 style={{
                     height: `calc(100dvh - ${ keyboardHeight}px)`,
-                    transition: "height 0.3s ease",
+                    // transition: "height 0.3s ease",
                 }}
     >
         <div className="flex bg-nav py-1 px-2 font-bold items-center gap-2"
